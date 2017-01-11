954b727a4d8785cdc90c0ff37581fc98deb657f6
Tony Bowden <tony@tmtm.com>
2017-01-11 08:46:44 +0000

    Names are now in column 2
    The first column is now images.

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,11 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://es.wikipedia.org/wiki/Anexo:Senadores_nacionales_de_Argentina',
   after: '//span[@id="Senadores"]',
   before: '//span[@id="V.C3.A9ase_tambi.C3.A9n"]',
-  xpath: './/table//tr//td[1]//a[not(@class="new")]/@title',
-) 
+  xpath: './/table//tr//td[2]//a[not(@class="new")]/@title'
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { es: names })
-
